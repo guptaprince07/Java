494e185dbae2392eba269fcb49b65cb63ffd8147
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-08 11:01:35 +0530

    updated codes

--- practice.java
+++ practice.java
@@ -383,15 +383,30 @@ import java.util.*;
 //     }
 // }
 
+// class practice{
+//     public static void main(String args[]){
+//         int n=5;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n-i;j++){
+//                 System.out.print(" ");
+//             }
+//             for(int k=1;k<=i;k++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+//Half pyramid with numbers
 class practice{
     public static void main(String args[]){
-        int n=5;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter value of n: ");
+        int n=sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            for(int k=1;k<=i;k++){
-                System.out.print("*");
+            for(int j=1;j<=i;j++){
+                System.out.print(" "+j);
             }
             System.out.println();
         }
