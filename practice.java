b32eb9204f0d16d8f45f87bb2ca114e834ee6d84
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-08 11:31:49 +0530

    updated practice code

--- practice.java
+++ practice.java
@@ -399,14 +399,42 @@ import java.util.*;
 // }
 
 //Half pyramid with numbers
+// class practice{
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter value of n: ");
+//         int n=sc.nextInt();
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(" "+j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+//Inverted half pyramid with numbers
+// class practice{
+//     public static void main(String args[]){
+//         int n=5;
+//         for(int i=n;i>=1;i--){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+//Flody's Triangle
 class practice{
     public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter value of n: ");
-        int n=sc.nextInt();
+        int n=5;
+        int number=1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(" "+j);
+                System.out.print(" "+number);
+                number++;
             }
             System.out.println();
         }
