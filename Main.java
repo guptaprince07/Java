637d045c1f450e222591924a6411df4d5fb7f9b9
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-09 19:10:55 +0530

    updated main codes

--- Main.java
+++ Main.java
@@ -525,16 +525,28 @@ import java.util.*;
 //     }
 // }
 
-class Main {
+// class Main {
+//     public static void main(String[] args) {
+//         int n = 5; 
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= (n - i); j++) {
+//                 System.out.print(" ");
+//             }
+//             for (int k = 1; k <= (2 * i - 1); k++) {
+//                 System.out.print("*");}
+//             System.out.println();
+//         }
+//     }
+// }
+
+class Main{
     public static void main(String[] args) {
-        int n = 5; 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= (n - i); j++) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");}
-            System.out.println();
-        }
+        int a=4;
+        int b=12;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
