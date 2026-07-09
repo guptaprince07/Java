e664ea0122264d34f2e724947668223d10b1429b
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-09 18:27:00 +0530

    updated

--- Main.java
+++ Main.java
@@ -475,12 +475,50 @@ import java.util.*;
 //     }
 // }
 
+// class Main{
+//     public static void main(String args[]){
+//         int n=5;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// class Main{
+//     public static void main(String args[]){
+//         int n=5;
+//         for(int i=5;i>=0;i--){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// class Main{
+//     public static void main(String args[]){
+//         int n=5;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(i);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
 class Main{
-    public static void main(String args[]){
-        int n=5;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(j);
+    public static void main(String[] args) {
+        int n=4;
+        int number=1;
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(" "+number);
+                number++;
             }
             System.out.println();
         }
