a412a434b01d8c1e5b2189e1a722cb4906e993e0
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-10 15:25:01 +0530

    updated

--- practice.java
+++ practice.java
@@ -567,16 +567,29 @@ import java.util.*;
 //     }
 // }
 
-class practice {
+// class practice {
+//     public static void main(String[] args) {
+//         int n = 5; 
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= (n - i); j++) {
+//                 System.out.print(" ");
+//             }
+//             for (int k = 1; k <= (2 * i - 1); k++) {
+//                 System.out.print("*");}
+//             System.out.println();
+//         }
+//     }
+// }
+
+//Functions
+class practice{
+    public static void name(String name){
+    System.out.println("Your name is: "+name);
+    }
     public static void main(String[] args) {
-        int n = 5; 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= (n - i); j++) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");}
-            System.out.println();
-        }
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your name here: ");
+        String name=sc.nextLine();
+        name(name);
     }
 }
