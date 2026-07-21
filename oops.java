3b9bae98ff693e9c28c6472ca00023e313ac64eb
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-21 11:40:56 +0530

    updated

--- oops.java
+++ oops.java
@@ -9,6 +9,17 @@ class Pen{
         System.out.println(this.color);
     }
 }
+class Student{
+    String name;
+    int age;
+    int rollno;
+    public void print(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+        System.out.println(this.rollno);
+    }
+}
+
 public class oops{
     public static void main(String[] args) {
         Pen pen1=new Pen();
@@ -22,5 +33,11 @@ public class oops{
         pen2.type="Ball";
         pen2.write();
         pen2.color();
+
+        Student s1=new Student();
+        s1.name="abc";
+        s1.age=18;
+        s1.rollno=1234567890;
+        s1.print();
     }
 }
