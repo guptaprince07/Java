4c376518f7d9fd544018bece31ef4441022bf188
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-23 14:41:38 +0530

    practice

--- s1.java
+++ s1.java
@@ -13,16 +13,42 @@
 // }
 
 //Q-2
+// class s1{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         if(sc.hasNextInt()){
+//             int n=sc.nextInt();
+//             int total=0;
+//             for(int i=0;i<=n;i++){
+//                 total+=sc.nextInt();
+//             }
+//             System.out.println(total);
+//         }
+//     }
+// }
+
+//Q-3
 class s1{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        if(sc.hasNextInt()){
-            int n=sc.nextInt();
-            int total=0;
-            for(int i=0;i<=n;i++){
-                total+=sc.nextInt();
-            }
-            System.out.println(total);
+        int n=sc.nextInt();
+        boolean prime=true;
+        if(n<=1){
+            prime=false;
         }
+        else{
+            for(int i=2;i*i<=n;i++){
+                if(n%i==0){
+                    prime=false;
+                    break;
+                }
+            }     
+        }
+        if(prime){
+                System.out.println("Prime");
+            }
+            else{
+                System.out.println("Not prime");
+            }
     }
 }
