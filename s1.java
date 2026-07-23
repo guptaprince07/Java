acb3fd7fb2b3950fb3b0b8e99ed2273fff19377c
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-23 14:25:37 +0530

    practice

---
+++ s1.java
@@ -0,0 +1,15 @@
+ import java.util.*;
+//Q-1
+// class s1{
+//     public static void main(String [] arg){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         long fact=1;
+//         for(int i=1;i<=n;i++){
+//             fact*=i;
+//         }
+//         System.out.println(fact);
+//     }
+// }
+
+//Q-2
