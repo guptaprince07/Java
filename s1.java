57bb6299e50efc352710bddb44155ce9de9683a1
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-23 14:28:55 +0530

    practice

--- s1.java
+++ s1.java
@@ -13,3 +13,16 @@
 // }
 
 //Q-2
+class s1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        if(sc.hasNextInt()){
+            int n=sc.nextInt();
+            int total=0;
+            for(int i=0;i<=n;i++){
+                total+=sc.nextInt();
+            }
+            System.out.println(total);
+        }
+    }
+}
