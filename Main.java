8b5776bcc0cf9d24a58e2e0ef2d28fccfc460b6b
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-23 18:22:53 +0530

    updated

--- Main.java
+++ Main.java
@@ -1191,12 +1191,34 @@ import java.util.*;
 //     }
 // }
 
+// class Main{
+//     public static void main(String[] args) {
+//         for(int i=1;i<=100;i++){
+//             if(i%2!=0){
+//                 System.out.println(i);
+//             }
+//         }
+//     }
+// }
+
+// class Main{
+//     public static void main(String[] args) {
+//         for(int i=1;i<=1000;i++){
+//             if(i%7==0){
+//                 System.out.println(i);
+//             }
+//         }
+//     }
+// }
+
 class Main{
     public static void main(String[] args) {
-        for(int i=1;i<=100;i++){
-            if(i%2!=0){
-                System.out.println(i);
+        int n=7;
+        for(int i=n;i>=1;i=i-2){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
             }
+            System.out.println("");
         }
     }
 }
