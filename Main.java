be3160d88ee9f7005cfc01a732c14460e6d89da9
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-23 18:15:55 +0530

    updated

--- Main.java
+++ Main.java
@@ -1190,3 +1190,13 @@ import java.util.*;
 //         System.out.println(s.reverse());
 //     }
 // }
+
+class Main{
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
