6bba399aef20069e3cc04871e9b057f05b69429a
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-24 10:51:27 +0530

    updated

--- s1.java
+++ s1.java
@@ -89,19 +89,79 @@
 // }
 
 //anagram
- class s1 {
+//  class s1 {
+//     public static void main(String[] args) {
+//         String s1="heart";
+//         String s2="earth";
+//         char []a=s1.toCharArray();
+//         char []b=s2.toCharArray();
+//         Arrays.sort(a);
+//         Arrays.sort(b);
+//         if(Arrays.equals(a, b)){
+//             System.out.println("Anagram");
+//         }
+//         else{
+//             System.out.println("Non Anagram");
+//         }
+//     }
+// }
+
+//fibonanci
+// class s1{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int fibo[]=new int[n];
+//         if(n>=1){
+//             fibo[0]=0;
+//         }
+//         if(n>=2){
+//             fibo[1]=1;
+//         }
+//         for(int i=2;i<n;i++){
+//             fibo[i]=fibo[i-1]+fibo[i-2];
+//         }
+//         for(int i=0;i<n;i++){
+//             System.out.println(fibo[i]);
+//         }
+
+//     }
+//     }
+
+//max in array
+// class s1{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int array[]=new int[n];
+//         for(int i=0;i<n;i++){
+//             array[i]=sc.nextInt();
+//         }
+//         int max=array[0];
+//         for(int i=0;i<array.length-1;i++){
+//             if(array[i]>max){
+//                 max=array[i];
+//             }
+//         }
+//         System.out.println("max is: "+max);
+//     }
+// }
+
+//min in array
+class s1{
     public static void main(String[] args) {
-        String s1="heart";
-        String s2="earth";
-        char []a=s1.toCharArray();
-        char []b=s2.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        if(Arrays.equals(a, b)){
-            System.out.println("Anagram");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int array[]=new int[n];
+        for(int i=0;i<n;i++){
+            array[i]=sc.nextInt();
         }
-        else{
-            System.out.println("Non Anagram");
+        int min=array[0];
+        for(int i=array.length-1;i>=0;i--){
+            if(array[i]<min){
+                min=array[i];
+            }
         }
+        System.out.println("min is:"+min);
     }
 }
