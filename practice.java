13b6bd4287d071911c800c6e3cb360e8a680ac89
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-25 21:58:40 +0530

    updated

--- practice.java
+++ practice.java
@@ -1019,16 +1019,54 @@ import java.util.*;
 //     }
 // }
 
+// class practice{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         String s1=sc.next();
+//         String s2=sc.next();
+//         char []S1=s1.toCharArray();
+//         char []S2=s2.toCharArray();
+//         Arrays.sort(S1);
+//         Arrays.sort(S2);
+//         if(Arrays.equals(S1,S2)){
+//             System.out.println("Anagram");
+//         }
+//         else{
+//             System.out.println("Not-Anagram");
+//         }
+//     }
+// }
+
+// class practice{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int fibo[]=new int[n];
+//         if(n>=1){
+//             fibo[0]=0;
+//         }
+//         if(n>=2){
+//             fibo[1]=1;
+//         }
+//         for(int i=2;i<n;i++){
+//             fibo[i]=fibo[i-1]+fibo[i-2];
+//         }
+//         for(int i=0;i<n;i++){
+//             System.out.println(fibo[i]);
+//         }
+//     }
+// }
+
 class practice{
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String s1=sc.next();
-        String s2=sc.next();
-        char []S1=s1.toCharArray();
-        char []S2=s2.toCharArray();
-        Arrays.sort(S1);
-        Arrays.sort(S2);
-        if(Arrays.equals(S1,S2)){
+        Scanner sc=new Scanner (System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        char a[]=s1.toCharArray();
+        char b[]=s2.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if(Arrays.equals(a,b)){
             System.out.println("Anagram");
         }
         else{
