d9cc3e10dfa4da1e824871fe7d0d1fe5d2223ffd
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-27 16:49:33 +0530

    2-D Array

--- Main.java
+++ Main.java
@@ -1254,3 +1254,24 @@ import java.util.*;
 //             }
 //         }
 //     }}
+
+//2-D array
+// class Main{
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         int arr[][]=new int[3][3];
+//         for(int i=0;i<3;i++){
+//             for(int j=0;j<3;j++){
+//                 arr[i][j]=sc.nextInt();
+//             }
+//         }
+//         for(int i=0;i<3;i++){
+//             for(int j=0;j<3;j++){
+//                 System.out.println(arr[i][j]+"");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+//sum of diagonal elements in 2-d array
