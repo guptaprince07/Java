d9f76a8a0c8f98779cc09b329eda384848b4b7d1
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-28 11:48:35 +0530

    updated array codes

--- Main.java
+++ Main.java
@@ -1350,5 +1350,33 @@ import java.util.*;
 // }
 
 //sum of 2 matrx 
+class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int mat1[][]=new int[r][c];
+        int mat2[][]=new int[r][c];
+        int sum[][]=new int [r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                mat1[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                mat2[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                sum[i][j]=mat1[i][j]+mat2[i][j];
+                System.out.print(sum[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
 //sum of each row and column
 //determinant of 2-d array
+//q-48 leetcode rotate a matrix
