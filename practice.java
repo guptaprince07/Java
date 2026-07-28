4d3e1604090d23a93c3c4f156907e136cb3097db
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-07-28 11:42:08 +0530

    2-d array

--- practice.java
+++ practice.java
@@ -1057,20 +1057,41 @@ import java.util.*;
 //     }
 // }
 
+// class practice{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner (System.in);
+//         String s1=sc.nextLine();
+//         String s2=sc.nextLine();
+//         char a[]=s1.toCharArray();
+//         char b[]=s2.toCharArray();
+//         Arrays.sort(a);
+//         Arrays.sort(b);
+//         if(Arrays.equals(a,b)){
+//             System.out.println("Anagram");
+//         }
+//         else{
+//             System.out.println("Not-Anagram");
+//         }
+//     }
+// }
+
 class practice{
     public static void main(String[] args) {
-        Scanner sc=new Scanner (System.in);
-        String s1=sc.nextLine();
-        String s2=sc.nextLine();
-        char a[]=s1.toCharArray();
-        char b[]=s2.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        if(Arrays.equals(a,b)){
-            System.out.println("Anagram");
+        Scanner sc=new Scanner(System.in);
+        int arr[][]=new int[4][4];
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                arr[i][j]=sc.nextInt();
+            }
         }
-        else{
-            System.out.println("Not-Anagram");
+        int n=arr.length;
+        int leftsum=0;
+        int rightsum=0;
+        for(int k=0;k<n;k++){
+            leftsum+=arr[k][k];
+            rightsum=arr[k][n-1-k];
         }
+        System.out.println("left diagonal sum: "+leftsum);
+        System.out.println("right diagonal sum: "+rightsum);
     }
 }
