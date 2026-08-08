1cbc0c9ef470f2513dcef1eca5338c30f018415a
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-08-08 15:00:06 +0530

    update

--- oops.java
+++ oops.java
@@ -1,65 +1,66 @@
-// class Pen{
-//     String color;
-//     String type;
+// // class Pen{
+// //     String color;
+// //     String type;
 
-//     public void write(){
-//         System.out.println("pen is working");
-//     }
-//     public void color(){
-//         System.out.println(this.color);
-//     }
-// }
-// class Student{
-//     String name;
-//     int age;
-//     int rollno;
-//     public void print(){
-//         System.out.println(this.name);
-//         System.out.println(this.age);
-//         System.out.println(this.rollno);
-//     }
-//     Student(String name,int age,int rollno){
-//         this.name=name;
-//         this.age=age;
-//         this.rollno=rollno;
-//     }//student constructor
-// }
+// //     public void write(){
+// //         System.out.println("pen is working");
+// //     }
+// //     public void color(){
+// //         System.out.println(this.color);
+// //     }
+// // }
+// // class Student{
+// //     String name;
+// //     int age;
+// //     int rollno;
+// //     public void print(){
+// //         System.out.println(this.name);
+// //         System.out.println(this.age);
+// //         System.out.println(this.rollno);
+// //     }
+// //     Student(String name,int age,int rollno){
+// //         this.name=name;
+// //         this.age=age;
+// //         this.rollno=rollno;
+// //     }//student constructor
+// // }
+
+// // public class oops{
+// //     public static void main(String[] args) {
+// //         Pen pen1=new Pen();
+// //         pen1.color="Black";
+// //         pen1.type="Gel";
+// //         pen1.write();
+// //         pen1.color();
+
+// //         Pen pen2=new Pen();
+// //         pen2.color="Blue";
+// //         pen2.type="Ball";
+// //         pen2.write();
+// //         pen2.color();
+
+// //         Student s1=new Student("abc",18,123456789); //by using constructor
+// //         // s1.name="abc";
+// //         // s1.age=18;
+// //         // s1.rollno=1234567890;
+// //         s1.print();
+// //     }
+// // }
 
 // public class oops{
-//     public static void main(String[] args) {
-//         Pen pen1=new Pen();
-//         pen1.color="Black";
-//         pen1.type="Gel";
-//         pen1.write();
-//         pen1.color();
+//     public int age;
+//     public String name;
+//     public int id;
+//     public int subs;
 
-//         Pen pen2=new Pen();
-//         pen2.color="Blue";
-//         pen2.type="Ball";
-//         pen2.write();
-//         pen2.color();
-
-//         Student s1=new Student("abc",18,123456789); //by using constructor
-//         // s1.name="abc";
-//         // s1.age=18;
-//         // s1.rollno=1234567890;
-//         s1.print();
+//     public void study(){
+//         System.out.println(name+"Studying");
+//     }
+//     public void bunk(){
+//         System.out.println(name+"bunk");
+//     }
+//     public void sleep(){
+//         System.out.println(name+"sleeping");
 //     }
 // }
 
-public class oops{
-    public int age;
-    public String name;
-    public int id;
-    public int subs;
-
-    public void study(){
-        System.out.println(name+"Studying");
-    }
-    public void bunk(){
-        System.out.println(name+"bunk");
-    }
-    public void sleep(){
-        System.out.println(name+"sleeping");
-    }
-}
