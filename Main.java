11614dfec04c9c5d8a73167ed6ce251714f2e7cd
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-08-08 14:05:11 +0530

    updated

--- Main.java
+++ Main.java
@@ -1552,24 +1552,24 @@ import java.util.*;
 // }
 
 //binary search
-public class Main {
-    public static int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+// public class Main {
+//     public static int search(int[] nums, int target) {
+//         int left = 0;
+//         int right = nums.length - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2; // Prevents integer overflow
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2; // Prevents integer overflow
 
-            if (nums[mid] == target) {
-                return mid; // Target found at index mid
-            } else if (nums[mid] < target) {
-                left = mid + 1; // Search in the right half
-            } else {
-                right = mid - 1; // Search in the left half
-            }
-        }
+//             if (nums[mid] == target) {
+//                 return mid; // Target found at index mid
+//             } else if (nums[mid] < target) {
+//                 left = mid + 1; // Search in the right half
+//             } else {
+//                 right = mid - 1; // Search in the left half
+//             }
+//         }
 
-        return -1; // Target not found
-    }
-}
+//         return -1; // Target not found
+//     }
+// }
 
