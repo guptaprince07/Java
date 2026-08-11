1f7ab339501cb67cfd0646df9e4f4ee2c966a5d1
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-08-11 14:25:00 +0530

    practice\

--- s2.java
+++ s2.java
@@ -45,17 +45,39 @@ import java.util.*;
 //q-3
 
 //q-4
+// class s2{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         String s=sc.nextLine();
+//         char ch=sc.next().charAt(0);
+//         int count=0;
+//         for(int i=0;i<s.length();i++){
+//             if(s.charAt(i)==ch){
+//                 count++;
+//             }
+//         }
+//         System.out.printf("%c occured %d times",ch,count);
+//     }
+// }
+
+//q-5
+//q-6
+
+//q-7
 class s2{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine();
-        char ch=sc.next().charAt(0);
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==ch){
-                count++;
-            }
+        String s=sc.next();
+        String rev="";
+        String temp=s;
+        for(int i=s.length()-1;i>=0;i--){
+            rev+=s.charAt(i);
         }
-        System.out.printf("%c occured %d times",ch,count);
+        if(temp.equals(rev)){
+            System.out.println("Pallindrome");
+        }
+        else{
+            System.out.println("Not Pallindrome");
+        }
     }
 }
