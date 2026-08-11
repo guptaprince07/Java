901fb0919e8e321c81262e146971cc7f5bfffa0a
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-08-11 15:09:57 +0530

    update ds2

--- s2.java
+++ s2.java
@@ -106,24 +106,24 @@ import java.util.*;
 // }
 
 //q-15
-class s2{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int r=sc.nextInt();
-        int c=sc.nextInt();
-        int arr[][]=new int[r][c];
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                arr[i][j]=sc.nextInt();
-            }
-        }
-        for(int i=0;i<arr.length;i++){
-            int rowsum=0;
-            for(int j=0;j<arr[i].length;j++){
-                rowsum+=arr[i][j];  
-            }
-            System.out.println(rowsum);
-        }
-    }
-}
+// class s2{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int r=sc.nextInt();
+//         int c=sc.nextInt();
+//         int arr[][]=new int[r][c];
+//         for(int i=0;i<r;i++){
+//             for(int j=0;j<c;j++){
+//                 arr[i][j]=sc.nextInt();
+//             }
+//         }
+//         for(int i=0;i<arr.length;i++){
+//             int rowsum=0;
+//             for(int j=0;j<arr[i].length;j++){
+//                 rowsum+=arr[i][j];  
+//             }
+//             System.out.println(rowsum);
+//         }
+//     }
+// }
 
