c73ad80428cee108d7615293ea36c44bcc402440
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-08-11 14:01:21 +0530

    practice

--- s2.java
+++ s2.java
@@ -1,18 +1,43 @@
 import java.util.*;
 
 //q-1
+// class s2{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         String s=sc.nextLine();
+//         int count=1;
+//         s=s.toUpperCase();
+//         for(int i=0;i<s.length();i++){
+//             if(s.charAt(i)==' '){
+//                 count++;
+//             }
+//         }
+//         System.out.println(s);
+//         System.out.println(count);
+//     }
+// }
+
+//q-2
+
 class s2{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine();
-        int count=1;
-        s=s.toUpperCase();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==' '){
-                count++;
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int arr[][]=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[i][j]=sc.nextInt();
             }
         }
-        System.out.println(s);
-        System.out.println(count);
+        int max=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(arr[i][j]>max){
+                    max=arr[i][j];
+                }
+            }
+        }
+        System.out.println("max is: "+max);
     }
 }
