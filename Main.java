067012a954d4e910eac0035de67d7999757a42da
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-08-11 17:15:47 +0530

    updated

--- Main.java
+++ Main.java
@@ -1574,6 +1574,28 @@ import java.util.*;
 // }
 
 //row-wise sum of matrix
+// class Main{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int r=sc.nextInt();
+//         int c=sc.nextInt();
+//         int arr[][]=new int[r][c];
+//         for(int i=0;i<r;i++){
+//             for(int j=0;j<c;j++){
+//                 arr[i][j]=sc.nextInt();
+//             }
+//         }
+//         for(int i=0;i<arr.length;i++){
+//             int rowsum=0;
+//             for(int j=0;j<arr[0].length;j++){
+//                 rowsum+=arr[i][j];
+//             }
+//             System.out.println(rowsum);
+//         }
+//     }
+// }
+
+//colum-wise sum
 class Main{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -1585,12 +1607,12 @@ class Main{
                 arr[i][j]=sc.nextInt();
             }
         }
-        for(int i=0;i<arr.length;i++){
-            int rowsum=0;
-            for(int j=0;j<arr[0].length;j++){
-                rowsum+=arr[i][j];
+        for(int j=0;j<c;j++){
+            int csum=0;
+            for(int i=0;i<r;i++){
+                csum+=arr[i][j];
             }
-            System.out.println(rowsum);
+            System.out.println(csum);
         }
     }
 }
