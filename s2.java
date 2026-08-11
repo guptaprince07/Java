6268b4375a83f2d499eb48b724d5f7eaaef4207f
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-08-11 14:10:07 +0530

    practice

--- s2.java
+++ s2.java
@@ -19,25 +19,43 @@ import java.util.*;
 
 //q-2
 
+// class s2{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int r=sc.nextInt();
+//         int c=sc.nextInt();
+//         int arr[][]=new int[r][c];
+//         for(int i=0;i<r;i++){
+//             for(int j=0;j<c;j++){
+//                 arr[i][j]=sc.nextInt();
+//             }
+//         }
+//         int max=0;
+//         for(int i=0;i<r;i++){
+//             for(int j=0;j<c;j++){
+//                 if(arr[i][j]>max){
+//                     max=arr[i][j];
+//                 }
+//             }
+//         }
+//         System.out.println("max is: "+max);
+//     }
+// }
+
+//q-3
+
+//q-4
 class s2{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int r=sc.nextInt();
-        int c=sc.nextInt();
-        int arr[][]=new int[r][c];
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                arr[i][j]=sc.nextInt();
+        String s=sc.nextLine();
+        char ch=sc.next().charAt(0);
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==ch){
+                count++;
             }
         }
-        int max=0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(arr[i][j]>max){
-                    max=arr[i][j];
-                }
-            }
-        }
-        System.out.println("max is: "+max);
+        System.out.printf("%c occured %d times",ch,count);
     }
 }
