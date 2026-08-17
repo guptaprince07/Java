d0e28e1bfc1dd09ebf5039ef0a7d89043bc6881d
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-08-17 16:33:22 +0530

    updated

--- Main.java
+++ Main.java
@@ -1668,3 +1668,34 @@ import java.util.*;
 //         return res;
 //     }
 // }
+
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        int n=mat.length;
+        for(int k=0;k<4;k++){
+            for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int left=0;
+            int right=n-1;
+            while(left<right){
+                int temp=mat[i][left];
+                mat[i][left]=mat[i][right];
+                mat[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
+        if(Arrays.deepEquals(mat,target)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+//create class where you had to create a custom exception
