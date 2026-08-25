4d639efacddc5c73ab8c9af61913b73b8a76cbaa
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-08-25 15:22:23 +0530

    updated

--- Main.java
+++ Main.java
@@ -1804,23 +1804,37 @@ import java.util.*;
 //         }
 // }
 
-class Main{
-        public static void main(String[] args) {
-                Scanner sc=new Scanner(System.in);
-                int r=sc.nextInt();
-                int c=sc.nextInt();
-                int arr[][]=new int[r][c];
-                for(int i=0;i<r;i++){
-                        for(int j=0;j<c;j++){
-                        arr[i][j]=sc.nextInt();
-                        }
-        }
-         for(int i=0;i<r;i++){
-                int rsum=0;
-                for(int j=0;j<c;j++){
-                        rsum+=arr[i][j];
-                }System.out.println(rsum);
-         }        
-}
-}
+// class Main{
+//         public static void main(String[] args) {
+//                 Scanner sc=new Scanner(System.in);
+//                 int r=sc.nextInt();
+//                 int c=sc.nextInt();
+//                 int arr[][]=new int[r][c];
+//                 for(int i=0;i<r;i++){
+//                         for(int j=0;j<c;j++){
+//                         arr[i][j]=sc.nextInt();
+//                         }
+//         }
+//          for(int i=0;i<r;i++){
+//                 int rsum=0;
+//                 for(int j=0;j<c;j++){
+//                         rsum+=arr[i][j];
+//                 }System.out.println(rsum);
+//          }        
+// }
+// }
 
+class Main {
+        public static void main(String[] args) {
+                String s=new String("Programming");
+                int freq[]=new int[256];
+                for(int i=0;i<s.length();i++){
+                        freq[s.charAt(i)]++;
+                }
+                for(int i=0;i<256;i++){
+                        if(freq[i]>0){
+                                System.out.println("'" + (char) i + "' : " + freq[i]);
+                        }
+                }
+        }
+}
