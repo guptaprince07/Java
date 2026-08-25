a7ad8ed6dbfae7db9d502e8ec163b11d4fed2c66
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-08-25 14:53:28 +0530

    u[dated

--- Main.java
+++ Main.java
@@ -1786,20 +1786,40 @@ import java.util.*;
 //         }
 // }
 
+// class Main{
+//         public static void main(String[] args) {
+//                 Scanner sc=new Scanner(System.in);
+//                 String s1=sc.next();
+//                 String s2=sc.next();
+//                 char a[]=s1.toCharArray();
+//                 char b[]=s2.toCharArray();
+//                 Arrays.sort(a);
+//                 Arrays.sort(b);
+//                 if(Arrays.equals(a,b)){
+//                         System.out.println("Anagram");
+//                 }
+//                 else{
+//                         System.out.println("Not Anargram");
+//                 }
+//         }
+// }
+
 class Main{
         public static void main(String[] args) {
                 Scanner sc=new Scanner(System.in);
-                String s1=sc.next();
-                String s2=sc.next();
-                char a[]=s1.toCharArray();
-                char b[]=s2.toCharArray();
-                Arrays.sort(a);
-                Arrays.sort(b);
-                if(Arrays.equals(a,b)){
-                        System.out.println("Anagram");
-                }
-                else{
-                        System.out.println("Not Anargram");
-                }
+                int r=sc.nextInt();
+                int c=sc.nextInt();
+                int arr[][]=new int[r][c];
+                for(int i=0;i<r;i++){
+                        for(int j=0;j<c;j++){
+                        arr[i][j]=sc.nextInt();
+                        }
         }
+         for(int i=0;i<r;i++){
+                int rsum=0;
+                for(int j=0;j<c;j++){
+                        rsum+=arr[i][j];
+                }System.out.println(rsum);
+         }        
+}
 }
