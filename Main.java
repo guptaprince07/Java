a6734260296a4ccaa59cf24aedbeff3a19ad3b84
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-08-26 15:11:38 +0530

    updated

--- Main.java
+++ Main.java
@@ -1824,17 +1824,17 @@ import java.util.*;
 // }
 // }
 
-class Main {
-        public static void main(String[] args) {
-                String s=new String("Programming");
-                int freq[]=new int[256];
-                for(int i=0;i<s.length();i++){
-                        freq[s.charAt(i)]++;
-                }
-                for(int i=0;i<256;i++){
-                        if(freq[i]>0){
-                                System.out.println("'" + (char) i + "' : " + freq[i]);
-                        }
-                }
-        }
-}
+// class Main {
+//         public static void main(String[] args) {
+//                 String s=new String("Programming");
+//                 int freq[]=new int[256];
+//                 for(int i=0;i<s.length();i++){
+//                         freq[s.charAt(i)]++;
+//                 }
+//                 for(int i=0;i<256;i++){
+//                         if(freq[i]>0){
+//                                 System.out.println("'" + (char) i + "' : " + freq[i]);
+//                         }
+//                 }
+//         }
+// }
