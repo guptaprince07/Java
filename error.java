e88638432e0b9cf162044871585f9b2f3a46038a
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-09-01 10:41:37 +0530

    exception handling

--- error.java
+++ error.java
@@ -1,15 +1,46 @@
 import java.util.*;
+// class error{
+//     public static void main(String args[]){
+//         int a=20;
+//         int b=0;
+//         try{
+//             int c=a/b;
+//             System.out.println("res is:"+c);
+//         }
+//         catch(Exception e){
+//             System.out.println("failed to divide.");
+//             System.out.println("reason"+e);
+//         }
+//     }
+// }
+
 class error{
-    public static void main(String args[]){
-        int a=20;
-        int b=0;
+    public static void main(String[] args) {
+        int marks[]=new int[3];
+        marks[0]=75;
+        marks[1]=89;
+        marks[2]=99;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter array index: ");
+        int index=sc.nextInt();
+        System.out.println("number divide: ");
+        int num=sc.nextInt();
+
         try{
-            int c=a/b;
-            System.out.println("res is:"+c);
+            System.out.println("value of entered index: "+marks[index]);
+            System.out.println("divide result:"+marks[index]/num);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexoutOfbound occured");
+            System.out.println("reason: "+e);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Aritematic error occured");
+            System.out.println("reason: "+e);
         }
         catch(Exception e){
-            System.out.println("failed to divide.");
-            System.out.println("reason"+e);
+            System.out.println("error occured");
+            System.out.println("reason: "+e);
         }
     }
 }
