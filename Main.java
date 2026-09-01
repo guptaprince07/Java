6a2874824e58aa09afe8ca2a98904362632d65b4
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-09-01 18:33:00 +0530

    updated

--- Main.java
+++ Main.java
@@ -1875,25 +1875,26 @@ import java.util.*;
 // }
 
 //LinkList
-class LL{
-        class Node{
-                String data;
-                Node next;
-        Node(String data){
-                this.data=data;
-                this.next=null;
-        }
-}
-public void addFirst(String data){
-        Node newNode=new Node(data);
-        if(head==null){
-                head=newNode;
-                return;
-        }
-        newNode.next=head;
-        head=newNode;
-}
-        public static void main(String args[]){
+// class LL{
+//         class Node{
+//                 String data;
+//                 Node next;
+//         Node(String data){
+//                 this.data=data;
+//                 this.next=null;
+//         }
+// }
+// public void addFirst(String data){
+//         Node newNode=new Node(data);
+//         if(head==null){
+//                 head=newNode;
+//                 return;
+//         }
+//         newNode.next=head;
+//         head=newNode;
+// }
+//         public static void main(String args[]){
 
-        }
-}
+//         }
+// }
+
