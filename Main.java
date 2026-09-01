9360ec415628c0eebf3fbd761f033b569097eae2
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-09-01 10:31:26 +0530

    updated

--- Main.java
+++ Main.java
@@ -1884,7 +1884,16 @@ class LL{
                 this.next=null;
         }
 }
+public void addFirst(String data){
+        Node newNode=new Node(data);
+        if(head==null){
+                head=newNode;
+                return;
+        }
+        newNode.next=head;
+        head=newNode;
+}
         public static void main(String args[]){
-                
+
         }
 }
