86695fe143104e1b82e52fc3610f1d9138b9dd82
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-09-02 10:14:04 +0530

    updated

--- practice.java
+++ practice.java
@@ -1257,16 +1257,34 @@ import java.util.*;
 //     }
 // }
 
+// class practice{
+//     public static void main(String args[]){
+//         Scanner sc= new Scanner(System.in);
+//         ArrayList<Integer>arr=new ArrayList<>();
+//         int n=sc.nextInt();
+//         for(int i=0;i<n;i++){
+//             arr.add(sc.nextInt());
+//         }
+//         for(int i=n-1;i>=0;i--){
+//             System.out.print(arr.get(i)+" ");
+//         }
+//     }
+// }
+
 class practice{
-    public static void main(String args[]){
-        Scanner sc= new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         ArrayList<Integer>arr=new ArrayList<>();
         int n=sc.nextInt();
         for(int i=0;i<n;i++){
             arr.add(sc.nextInt());
         }
-        for(int i=n-1;i>=0;i--){
-            System.out.print(arr.get(i)+" ");
+        float avg=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr.get(i);
         }
+        avg=sum/n;
+        System.out.printf("Avg is:%.2f ",avg);
     }
 }
