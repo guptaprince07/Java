db79fd5d707f8df40f67711e3dd268a4ced5a462
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-09-04 15:14:08 +0530

    Hash Set

--- Main.java
+++ Main.java
@@ -1953,8 +1953,26 @@ import java.util.*;
 //first non repeating character given a string 
 //write a java program using a hashmap to find first char that appears only once
 
+
+//Hash Set
 class Main{
         public static void main(String[] args) {
-                
+            HashSet<Integer>set=new HashSet<>();
+            set.add(1);
+            set.add(5);
+            set.add(4);
+            set.add(1);//duplicates not allowed in set
+            
+            if(set.contains(1)){
+                System.out.println("contains 1");
+            }
+            if(!set.contains(10)){
+                System.out.println("doesn't contain");
+            }
+            set.remove(1);
+            if(!set.contains(1)){
+                System.out.println("deleted");
+            }
+            System.out.println(set.size());
         }
 }
