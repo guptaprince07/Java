f5d41b9c97881aabfcdd860ff568cbc687bd2113
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-09-07 16:49:03 +0530

    updated

--- error.java
+++ error.java
@@ -222,3 +222,18 @@ import java.util.*;
 //         }
 //     }
 // }
+
+class error{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        try{
+            int c=a/b;
+            System.out.println(c);
+        }
+        catch(ArithmeticException e){
+            System.out.println(e);
+        }
+    }
+}
