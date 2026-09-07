2f3e6363e9b8899c7e30ec28796ba7d2436e3289
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-09-07 11:12:41 +0530

    practice exception handling

--- error.java
+++ error.java
@@ -45,17 +45,35 @@ import java.util.*;
 //     }
 // }
 
-class error{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        try{
-            int c=a/b;
-            System.out.println(c);
-        }
-        catch(ArithmeticException e){
-            System.out.println("division by zero is not valid");
-        }
-    }
-}
+// class error{
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+//         try{
+//             int c=a/b;
+//             System.out.println(c);
+//         }
+//         catch(ArithmeticException e){
+//             System.out.println("division by zero is not valid");
+//         }
+//     }
+// }
+
+// class error{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int arr[]=new int[n];
+//         for(int i=0;i<n;i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         int index=sc.nextInt();
+//         try{
+//                 System.out.println(arr[index]);
+//         }
+//         catch(ArrayIndexOutOfBoundsException e){
+//                     System.out.println(e);
+//                 }
+//     }
+// }
