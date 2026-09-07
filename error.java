75638eef1270ff9b0fc7247be06b7d0ebf89f54f
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-09-07 15:09:19 +0530

    updated

--- error.java
+++ error.java
@@ -201,24 +201,24 @@ import java.util.*;
 //     }
 // }
 
-class error{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int arr[]={10,20,30};
-        int index=sc.nextInt();
-        try{
-            int c=a/b;
-            System.out.println("res:"+c);
+// class error{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+//         int arr[]={10,20,30};
+//         int index=sc.nextInt();
+//         try{
+//             int c=a/b;
+//             System.out.println("res:"+c);
 
-            System.out.println("arr value at index is:"+arr[index]);
-        }
-        catch(ArithmeticException e){
-            System.out.println("can't divide by zero");
-        }
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println(e);
-        }
-    }
-}
+//             System.out.println("arr value at index is:"+arr[index]);
+//         }
+//         catch(ArithmeticException e){
+//             System.out.println("can't divide by zero");
+//         }
+//         catch(ArrayIndexOutOfBoundsException e){
+//             System.out.println(e);
+//         }
+//     }
+// }
