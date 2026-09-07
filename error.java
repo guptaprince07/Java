a114da252f7b4308cc4a4c0c4a5e9df14a80fbea
guptaprince07 <prince.17.gupta.05@gmail.com>
2026-09-07 11:01:15 +0530

    practice exception handling

--- error.java
+++ error.java
@@ -14,33 +14,48 @@ import java.util.*;
 //     }
 // }
 
-class error{
-    public static void main(String[] args) {
-        int marks[]=new int[3];
-        marks[0]=75;
-        marks[1]=89;
-        marks[2]=99;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter array index: ");
-        int index=sc.nextInt();
-        System.out.println("number divide: ");
-        int num=sc.nextInt();
+// class error{
+//     public static void main(String[] args) {
+//         int marks[]=new int[3];
+//         marks[0]=75;
+//         marks[1]=89;
+//         marks[2]=99;
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("enter array index: ");
+//         int index=sc.nextInt();
+//         System.out.println("number divide: ");
+//         int num=sc.nextInt();
 
+//         try{
+//             System.out.println("value of entered index: "+marks[index]);
+//             System.out.println("divide result:"+marks[index]/num);
+//         }
+//         catch(ArrayIndexOutOfBoundsException e){
+//             System.out.println("ArrayIndexoutOfbound occured");
+//             System.out.println("reason: "+e);
+//         }
+//         catch(ArithmeticException e){
+//             System.out.println("Aritematic error occured");
+//             System.out.println("reason: "+e);
+//         }
+//         catch(Exception e){
+//             System.out.println("error occured");
+//             System.out.println("reason: "+e);
+//         }
+//     }
+// }
+
+class error{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
         try{
-            System.out.println("value of entered index: "+marks[index]);
-            System.out.println("divide result:"+marks[index]/num);
-        }
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("ArrayIndexoutOfbound occured");
-            System.out.println("reason: "+e);
+            int c=a/b;
+            System.out.println(c);
         }
         catch(ArithmeticException e){
-            System.out.println("Aritematic error occured");
-            System.out.println("reason: "+e);
-        }
-        catch(Exception e){
-            System.out.println("error occured");
-            System.out.println("reason: "+e);
+            System.out.println("division by zero is not valid");
         }
     }
 }
